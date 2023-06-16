7435e13f952b51e8415b1b8bb1937b75979d2275
rascove <rascove@yahoo.com>
2023-06-16 15:41:17 +0000

    Azure Storage and SendGrid

--- itapps/settings.py
+++ itapps/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ADMINS = [('Satrya Fajri Pratama', 's.pratama@shu.ac.uk'), ('Caren Fernandes', 'caren.fernandes@shu.ac.uk')]
+ADMINS = [('Satrya Fajri Pratama', 's.pratama@shu.ac.uk')]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
